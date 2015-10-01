c4a3ef6ca332b3bab44d56936bac184b76f49d8b
bennie12345 <bmuijldert@gmail.com>
2015-10-01 11:03:58 +0200

    Added folder structure and menu/intro template

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buttons : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        Application.LoadLevel("Game");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void MainMenu() 
+    {
+        Application.LoadLevel("Menu");
+    }
+}
---
+++ Assets/Scripts/Intro_script.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Intro_script : MonoBehaviour {
+
+	// Use this for initialization
+    void Start()
+    {
+        StartCoroutine(Example());
+    }
+
+    IEnumerator Example()
+    {
+        yield return new WaitForSeconds(2);
+        Application.LoadLevel("Menu");
+    }
+
+    #if UNITY_EDITOR
+        void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Application.LoadLevel("Menu");
+            }
+        }
+    #endif
+}
