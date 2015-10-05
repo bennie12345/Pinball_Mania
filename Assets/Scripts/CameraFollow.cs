fde764183dcee5c99fe6e74f2d06d2f53af1f5d2
bennie12345 <bmuijldert@gmail.com>
2015-10-05 13:33:17 +0200

    Edited playingfield

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]private GameObject player;
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position/4;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,14 +3,14 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-    [SerializeField]private float speed;
+    [SerializeField]private float speed = 5;
 
-    void FixedUpdate()
+    void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
 
         GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
     }
