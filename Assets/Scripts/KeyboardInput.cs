48fdc1c7ed9f9d960ecd426c9bdb976716b08821
SuperSaiyan4 <joeywelvaadt@gmail.com>
2015-10-06 11:20:36 +0200

    6-08-2015 11:20

---
+++ Assets/Scripts/KeyboardInput.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardInput : MonoBehaviour {
+
+	void Update () {
+		if(Input.GetKey(KeyCode.A)) {
+			GameObject flipper = GameObject.FindWithTag("Flipper");
+			flipper.GetComponent<FlipperRotation>().Rotate(true, false);
+		}
+
+		if(Input.GetKey(KeyCode.L)) {
+			GameObject flipper = GameObject.FindWithTag("Flipper");
+			flipper.GetComponent<FlipperRotation>().Rotate(false, true);
+		}
+
+		if(Input.GetKey(KeyCode.Space)) {
+			GameObject pin = GameObject.FindWithTag("Pin");
+			pin.GetComponent<PinShoot>().Shoot();
+		}
+	}
+}
